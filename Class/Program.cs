ba307ba5c444aeb7b09aa84d278295854ea8fe75
furkanarikantr <furkanarikantr@gmail.com>
2023-12-22 21:53:03 +0300

    Update.

--- Class/Program.cs
+++ Class/Program.cs
@@ -4,21 +4,6 @@
     {
         static void Main(string[] args)
         {
-            /*  Ders2
-                
-
-               
-
-                
-
-                
-
-                
-                    
-                 
-                            
-
-            */
             Console.WriteLine("Class Yapısı.");
 
             /*Class Nedir ? Neden Class Yapısı Kullanılır ?
@@ -54,8 +39,19 @@
                 artık bir türdür. Bu durumda o türü kullanabilmek için class adını kullanmamız gereklidir. OrnekModel ornekModel; 
             */
 
+            /*Class İçerisinde Tanımlanan Class, Class Elemanı mıdır ?
+                    Class içerisindeki class, üst class'ın bir member'ı değildir. Bir class'ın member'larına class'ın object'i üzerinden 
+                erişebiliyoruz fakat class içerisindeki class'a member'ı olmadığı için erişemiyoruz.
+            */
+
+            /*Class Elemanlarına Açıklama Satırı Nasıl Eklenir ?
+                    /// <summary>
+                    /// Bu bir örnek class'tır.
+                    /// </summary>
+            */
+
             //Referans noktası oluşturuldu. OrnekModel türünde model adında bir referans noktası. Referans noktası alma işlemi. Stackt'te tutulur.
-            OrnekModel model;
+            //OrnekModel model;
 
             /*Class Members (Class Elemanları/Üyeleri) * Field * Property * Method * Indexer 
                     - Field : Object içerisinde veri depoladığımız/tuttuğumuz alanlardır. Class içerisindeki değişkenlerdir. Eğer bir değişken
@@ -81,10 +77,30 @@
                             get{} -> Property'den data istendiğinden tetiklenir.
                             set{} -> Property'e data gönderildiğinde tetiklenir.Gönderilen data'yı value keyword'ü ile yakalar.
                         }
-                        --
-            
+                        --Prop => Encapsulation yapılıp hiç bir işleme, duruma tutulmadan sade bir şekilde yapılmasıdır. Prop property'ler compile
+                        edildikten sonra arka planda kendi field'larını oluştururlar. Dolayısıyla prop property'si kullanılacaksa field tanımlamaya
+                        gerek yoktur.
+                        [erişim belirleyicisi][geri dönüş değeri][property adı]
+                        {
+                            get; -> Property'den data istendiğinden tetiklenir.
+                            set; -> Property'e data gönderildiğinde tetiklenir.Gönderilen data'yı value keyword'ü ile yakalar.
+                        }
+                        --Auto Property Initializers => Bir property'nin ilk datasını object oluşturulur oluşturulmaz verebiliriz.
+                        [erişim belirleyicisi][geri dönüş değeri][property adı]{ get; set; } = 15
+                        --Ref Readonly Returns => Bir class içerisindeki field'ı referansıyla döndürmemizi sağlayan ve biryandan da bu değişken
+                        değerini read only yapan özelliktir.
+                        --Computed Properties => İçerisinde işlem, algoritma taşıyan property'lerdir.
+                        --Expression-Bodied Properties => Tanımlanan property'de lambda expression kullanmamızı sağlayan söz dizimidir.
+                        --Init-Only Properties - Init Accessor => Init-Only Properties onject'in sadece ilk yaratılış anında data atanmasıdır.
+                        Böylece iş kuralı gereği run-time'da datası değişmemesi gereken object'ler için önlem alınmaktadır.
+                    - Method : Object'deki field data'larında yahut dışarıdan gelen data'larda işlem yapmamızı sağlayan temel programatik 
+                parçalardır. Method'lar ile property'ler arasındaki temel fark, property'lerin bir geri dönüş türü olmak zorundayken, method'ların
+                geri dönüş türü olmak zorunda değildir. void olabilirler.
+                    - Indexer : Object'e indexer özelliği kazandıran, property'ler ile aynı olan elemanlardır.
+                    [erişim belirleyicisi][geri dönüş değeri]this[parametreler]{get{} set{}}
             */
 
+            //Field
             /*Field
             MyClass m1 = new MyClass();     //MyClass türünden m1 referansı ile MyClass object'i oluşturuyoruz.
             m1.a = 10;                       //m1 referansı ile MyClass object'ine erişip, o object'in a field'ına ulaşıyoruz.
@@ -93,11 +109,42 @@
             m2.a = 20;                       //m2 referansı ile MyClass object'ine erişip, o object'in a field'ına ulaşıyoruz.
             */
 
-            /*Property*/
+            //Property
+            /*FullProperty
             MyClass myClass = new MyClass();
             Console.WriteLine(myClass.Yasi); //Object'teki field data'sını ekrana bastırır. Eğer atanmamışsa default data gelir. 0
             myClass.Yasi = 10;               //Object'deki field datasına göndirelen datayı enjekte eder.  
             Console.WriteLine(myClass.Yasi); //Object'teki field data'sını ekrana bastırır. Eğer atanmamışsa default data gelir. 10
+            */
+
+            /*Prop
+            MyClass myClass = new MyClass();
+            myClass.Yasi = 20;
+            Console.WriteLine(myClass.Yasi);
+            */
+
+            /*Auto Property Initializers
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.Yasi);
+            */
+
+            //Method
+            /*Method
+            MyClass myClass = new MyClass();
+            Console.WriteLine(myClass.X());
+            */
+
+            //Indexer
+            /*Indexer
+            MyClass myClass = new MyClass();
+            myClass[5] = 5;
+            */
+
+            /*Class İçerisindeki Class
+            MyClass2 myClass2 = new MyClass2();
+            MyClass2.MyClass3 myClass3 = new MyClass2.MyClass3(); //Class'taki class class member'ı olmadığı için referansı üzerinden gideriz.
+            */
+
 
         }
     }
@@ -105,15 +152,57 @@
     class MyClass
     {
         //Field
-        int yasi; 
-        string b;
+        int yasi;
+        string adi = "Furkan ARIKAN";
+        int s1 = 5;
+        int s2 = 15;
 
         //Property
-        public int Yasi //FullProperty
+        /*FullProperty
+        public int Yasi
         {
             get { return yasi; }
             set { yasi = value; }
         }
+        */
+
+        /*Prop
+        public int Yasi { get; set; }
+        */
+
+        /*Auto Property Initializers
+        public int Yasi { get; set; } = 15;
+        */
+
+        /*Ref Readonly Returns
+        public ref readonly string Adi => ref adi;
+        */
+
+        /*Computed Properties
+        public int Topla
+        {
+            get
+            {
+                return s1 + s2;
+            }
+        }
+        */
+
+        /*Expression-Bodied Properties
+        public string Cinsiyet { get; set; } = "Erkek";
+        */
+
+        /*Method
+        public int X()
+        {
+            return 0;
+        }
+        */
+
+        /*Indexer
+        public int this[int a] { get { return a; } set { } }
+        */
+
     }
 
     class OrnekModel
@@ -136,9 +225,56 @@
     class insideNamespaceClass
     {
         //class içi class'ı.
-        class nestedTypeClass 
+        class nestedTypeClass
         {
-            
+
+        }
+    }
+
+    /// <summary>
+    /// Bu bir örnek class'tır.
+    /// </summary>
+    class MyClass2
+    {
+        //Field
+        /// <summary>
+        /// Bu bir field'dır.
+        /// </summary>
+        int a;
+
+        //Prop
+        /// <summary>
+        /// Bu bir property'dir.
+        /// </summary>
+        public int MyAProperty { get; set; }
+        
+        //Method
+        /// <summary>
+        /// Bu bir method'dur.
+        /// </summary>
+        public void X()
+        {
+
+        }
+
+        //Indexer
+        /// <summary>
+        /// Bu bir indexer'dır.
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public int this[int a]
+        {
+            get { return 0; }
+        }
+
+        //Nested Class
+        /// <summary>
+        /// Bu bir nested class'tır.
+        /// </summary>
+        public class MyClass3
+        {
+
         }
     }
 }
