8aa6a30577b967ac901c3b5388e9486e1a44c6d9
furkanarikantr <furkanarikantr@gmail.com>
2024-01-01 20:10:51 +0300

    Destructor.

---
+++ Destructor/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Destructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Destructor(Finalizer) Yapısı");
+
+            /*Destructor Nedir ? (Finalizer Metot)
+                    Bir class'tan üretilmiş olan object imha edilirken otomatik olarak çağırılan method'dur. C# programlama dilinde destructor
+                sadece class yapılanmasında kullanılabilir ve bir class sadece bir adet destructor method'una sahip olabilir. Constructor'taki gibi
+                overloading işlemleri yapılamaz. Destructor'lar parametre alamaz.
+            */
+        }
+    }
+}
